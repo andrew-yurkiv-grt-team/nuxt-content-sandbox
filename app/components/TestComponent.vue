dbc96e1aa101b6bb65871205de771e0259fedda1
Andrew <a.yurkiv@grt-team.com>
2025-10-30 17:41:31 +0200

    Image support

--- app/components/TestComponent.vue
+++ app/components/TestComponent.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  testProp?: string
+}>()
+</script>
+
 <template>
   <div>
     <h1>Test component</h1>
     <slot name="title" />
     <h1>Test component end</h1>
+    <img v-if="testProp" :src="testProp" />
   </div>
 </template>
